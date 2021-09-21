5d3cc94263a2676096ab7423d5581a279a54e22a
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-21 15:25:18 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ function entrar(){
   })
    
   if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = 'https://giusande.github.io/Giusande/ '
+    window.location.href = 'https://giusande.github.io/Giufilme/'
     
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
