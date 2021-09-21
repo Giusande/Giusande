7677b44b705a74b1fbcccef9ea18c14fe255fe2c
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-21 17:46:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,4 +2,6 @@
 function entrar(){
     window.location.href = 'https://giusande.github.io/Giufilme/'
 }
-    
+function cadastrar(){
+        window.location.href = 'https://giusande.github.io/Giusande/'
+ }     
